b9ef374f19aa5b88811e921e5d3673dc09f0e052
Shadowwraze <127537948+Shadowwraze@users.noreply.github.com>
2025-01-29 19:25:28 +0300

    Add speaker

---
+++ source/library/speaker.cpp
@@ -0,0 +1,20 @@
+﻿#include "speaker.h"
+#include <stdio.h>
+#include <string>
+
+std::string FormatMessage(const char* name, const char* age)
+{
+    return std::string("Hi, my name is ") + name + ". I'm " + age + " years old. What about you?\n";
+}
+
+void LogNameAge(const char* name, const char* age)
+{
+    if (name && age)
+    {
+        printf("%s", FormatMessage(name, age).c_str());
+    }
+    else
+    {
+        printf("* INVALID PARAMETERS *\n");
+    }
+}
