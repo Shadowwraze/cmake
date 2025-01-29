4976e75357f9497012503278e613d2617158cf99
Shadowwraze <127537948+Shadowwraze@users.noreply.github.com>
2025-01-29 19:27:17 +0300

    Add app

---
+++ source/executable/app.cpp
@@ -0,0 +1,15 @@
+﻿#include "../library/speaker.h"
+
+int main(int argc, const char **argv)
+{
+    if (argc == 3)
+    {
+        LogNameAge(argv[1], argv[2]); // Вызываем правильную функцию
+        return 0;
+    }
+    else
+    {
+        LogNameAge("UNKNOWN", "0"); // Корректный вызов для некорректных параметров
+        return argc;
+    }
+}
